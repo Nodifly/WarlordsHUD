544b52e5f5044c9f3a9d771454ce37ccc856d33c
Dan <danbenison@gmail.com>
2017-04-29 21:33:56 +0100

    Changing a few things to stop healing/damage carrying over to the next game.

--- src/main/java/me/notifly/warlordshud/main/Main.java
+++ src/main/java/me/notifly/warlordshud/main/Main.java
@@ -1,12 +1,15 @@
 package me.notifly.warlordshud.main;
 
+import javafx.scene.input.ContextMenuEvent;
 import me.notifly.warlordshud.HudPropertyApi;
 import net.minecraft.client.Minecraft;
 import net.minecraft.util.ChatComponentText;
 import net.minecraft.util.EnumChatFormatting;
+import net.minecraft.util.EnumParticleTypes;
 import net.minecraftforge.client.ClientCommandHandler;
 import net.minecraftforge.client.event.ClientChatReceivedEvent;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.world.BlockEvent;
 import net.minecraftforge.fml.common.FMLCommonHandler;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
@@ -28,6 +31,7 @@ public class Main {
     public static Integer currentHealing = 0;
     public static Integer currentDamage = 0;
 
+
     @EventHandler
     public void init(FMLInitializationEvent event) {
 
@@ -40,17 +44,29 @@ public class Main {
     }
 
     @SubscribeEvent
+    public void onBreak(BlockEvent.BreakEvent event) {
+        Integer xPos = Minecraft.getMinecraft().thePlayer.getPosition().getX();
+        Integer yPos = Minecraft.getMinecraft().thePlayer.getPosition().getX();
+        Integer zPos = Minecraft.getMinecraft().thePlayer.getPosition().getX();
+        Minecraft.getMinecraft().thePlayer.addChatComponentMessage(new ChatComponentText(EnumChatFormatting.GREEN + "Break'd"));
+        Minecraft.getMinecraft().theWorld.spawnParticle(EnumParticleTypes.SMOKE_NORMAL, xPos + 0.5D, yPos + 1.0D, zPos + 0.5D, 0.0D, 0.0D, 0.0D, new int[0]);
+    }
+
+    @SubscribeEvent
     public void onChat(ClientChatReceivedEvent event) {
 
         String message = event.message.getUnformattedText();
 
         if (message.contains("The gates will fall in ")) {
 
+            currentHealing = 0;
+            currentDamage = 0;
+
             warlords = true;
 
         } else if (warlords == true) {
 
-            if (message.contains(" joined the lobby!")) {
+            if (message.contains("Reward Summary")) {
 
                 Minecraft.getMinecraft().thePlayer.addChatComponentMessage(new ChatComponentText(EnumChatFormatting.GREEN + "Healing: " + EnumChatFormatting.GREEN + currentHealing));
                 Minecraft.getMinecraft().thePlayer.addChatComponentMessage(new ChatComponentText(EnumChatFormatting.RED + "Damage: " + EnumChatFormatting.RED + currentDamage));
